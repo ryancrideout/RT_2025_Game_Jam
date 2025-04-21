174e67f6b2a1a92bc3b2615e34142315d53f9732
ryancrideout <ryancrideout@gmail.com>
2025-04-21 16:20:36 -0600

    Added a simple UI, need to do more work on it though.

---
+++ rolesreversed/grim_reaper/GrimReaperUI.gd
@@ -0,0 +1,6 @@
+extends Control
+
+@onready var promote_button = $HBoxContainer/PromoteButton
+@onready var stasis_button = $HBoxContainer/StasisButton
+@onready var haste_button = $HBoxContainer/HasteButton
+@onready var corpse_explosion_button = $HBoxContainer/CorpseExplosionButton
