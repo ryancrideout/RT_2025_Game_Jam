c7c4e33bf89544470b087a93b0ec6b344727e243
timofriedman <45775634+timofriedman@users.noreply.github.com>
2025-04-22 07:39:47 -0700

    fixing UI

--- rolesreversed/game_manager/game_manager.gd
+++ rolesreversed/game_manager/game_manager.gd
@@ -7,8 +7,8 @@ func _ready():
 
 func _on_play_button_pressed():
     # Spawn two agents with different positions and names
-    spawn_agent(Vector2(1000, 5000), "res://ai_agent/skeleton_castle.tscn", "SkeletonAgent")
-    spawn_agent(Vector2(9000, 5000), "res://ai_agent/human_castle.tscn", "HumanAgent")
+    spawn_agent(Vector2(3500, 5000), "res://ai_agent/skeleton_castle.tscn", "SkeletonAgent")
+    spawn_agent(Vector2(6500, 5000), "res://ai_agent/human_castle.tscn", "HumanAgent")
     
     print("Spawned two agents!")
     
