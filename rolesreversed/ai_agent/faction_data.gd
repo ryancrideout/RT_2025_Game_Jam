63d471226d65e692f9744b1cb033fc5598eb594d
timofriedman <45775634+timofriedman@users.noreply.github.com>
2025-04-22 18:36:25 -0700

    added aura to outposts, resource increment on timers

--- rolesreversed/ai_agent/faction_data.gd
+++ rolesreversed/ai_agent/faction_data.gd
@@ -19,3 +19,12 @@ class_name FactionData
 @export var faction_direction: int
 
 @export var resource_UI_node_label: NodePath
+
+@export var PRIMARY_RESOURCE_per_base : int
+@export var SECONDARY_RESOURCE_per_base : int
+
+@export var PRIMARY_RESOURCE_per_outpost : int
+@export var SECONDARY_RESOURCE_per_outpost : int
+
+@export var PRIMARY_RESOURCE_per_unit : int
+@export var SECONDARY_RESOURCE_per_unit : int
